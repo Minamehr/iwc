76a87e6b5ed8b73897b3add825b070e1b2b43bce
Minamehr <Minamehr@users.noreply.github.com>
2026-03-16 15:53:43 +0000

    deploy: 8d805de2ae09ba717e3a324533ccf8fb37b0f0da

---
+++ _astro/chunk-FMBD7UC4.D-CZJB1X.js
@@ -0,0 +1,15 @@
+import{_ as o}from"./MarkdownRenderer.Rph0RXQG.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8d805de2ae09ba717e3a324533ccf8fb37b0f0da"};var n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="1613db20-7086-44c4-8590-83d1e70078e3",e._sentryDebugIdIdentifier="sentry-dbid-1613db20-7086-44c4-8590-83d1e70078e3")}catch{}})();var i=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{i as g};
